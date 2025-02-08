f43bca46922ed4a5a1cd3a1083d039bf7dbf332e
NastyaTsimanovich97 <nastya.cimanovich.97@mail.ru>
2025-02-08 23:06:39 +0300

    fix: update skeleton line props

--- src/components/SkeletonLine.tsx
+++ src/components/SkeletonLine.tsx
@@ -1,6 +1,6 @@
 interface ISkeletonLineProps {
-  height: number;
-  width: number;
+  height?: number;
+  width?: number;
 }
 
 export function SkeletonLine({ height = 40, width = 250 }: ISkeletonLineProps) {
